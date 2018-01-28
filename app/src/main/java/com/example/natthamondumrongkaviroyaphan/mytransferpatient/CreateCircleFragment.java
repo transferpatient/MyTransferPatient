23efb31943f2b75d2a2aecc4dc09f31a77d58617
natthamon dumrongkaviroyaphan <>
2018-01-28 19:38:36 +0700

    initial create circle

--- app/src/main/java/com/example/natthamondumrongkaviroyaphan/mytransferpatient/CreateCircleFragment.java
+++ app/src/main/java/com/example/natthamondumrongkaviroyaphan/mytransferpatient/CreateCircleFragment.java
@@ -2,23 +2,57 @@ package com.example.natthamondumrongkaviroyaphan.mytransferpatient;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class CreateCircleFragment extends Fragment {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
-    public CreateCircleFragment() {
-    }
+import java.util.HashMap;
+import java.util.Map;
+
+public class CreateCircleFragment extends Fragment implements View.OnClickListener {
+    private Button btn;
+    private EditText groupName;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
         View rootView = inflater.inflate(R.layout.create_circle, container, false);
-
-
-
+        btn = (Button) rootView.findViewById(R.id.done_button);
+        btn.setOnClickListener(this);
+        groupName = rootView.findViewById(R.id.group_name);
         return rootView;
     }
 
+    @Override
+    public void onClick(View v) {
+        int i = v.getId();
+        if (i == R.id.done_button) {
+            FirebaseUser currentFirebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+            if(currentFirebaseUser != null) {
+                String userId = currentFirebaseUser.getUid();
+                createGroup(userId, groupName.getText().toString());
+            }
+        }
+    }
+
+    private void createGroup(String userId, String groupName) {
+        DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
+        DatabaseReference groups = mRootRef.child("groups");
+        String key = groups.push().getKey();
+
+        HashMap<String, Object> postValues = new HashMap<>();
+        postValues.put("createdById", userId);
+        postValues.put("groupName", groupName);
+
+        Map<String, Object> childUpdates = new HashMap<>();
+        childUpdates.put(key, postValues);
+        groups.updateChildren(childUpdates);
+    }
 }
